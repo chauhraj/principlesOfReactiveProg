25f17d328b5bd932b1a2ca0d6e80889a7413bbc7
chauhraj <rajiv.chauhan@gmail.com>
2013-11-08 02:15:51 -0500

    Removed the additional test

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -42,13 +42,6 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(eh) == l.drop(2).head
   }
 
-  property("duplicate  for github testing delete minimum 2 times") = forAll { (a: Int, b : Int, c : Int) =>
-    val l = (a :: b :: c :: Nil) sorted
-    val h = insert(c, insert(b, insert(a, empty)))
-    val eh = deleteMin(deleteMin(h))
-    findMin(eh) == l.drop(2).head
-  }
-  
   property("delete minimum should return value in sorted order") = forAll { (a1 : Int, a2: Int, a3: Int, a4: Int, a5: Int,a6: Int,a7: Int,a8: Int) =>
     val l = a1::a2::a3::a4::a5::a6::a7::a8::Nil
     val heap = l.foldLeft(empty)((h, e) => insert(e, h))
